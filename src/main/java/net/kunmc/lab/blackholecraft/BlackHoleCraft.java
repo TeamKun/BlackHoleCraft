b952ecac408fad9e7beb8510b06a37abb3d472e4
Tomo-dq <tomomodq69@gmail.com>
2021-03-26 19:51:46 +0900

    first commit

---
+++ src/main/java/net/kunmc/lab/blackholecraft/BlackHoleCraft.java
@@ -0,0 +1,16 @@
+package net.kunmc.lab.blackholecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlackHoleCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
